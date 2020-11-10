f4a150c5b73bc406a36b0d18086bca4e7e392f1f
Yuning You <46954127+yyou1996@users.noreply.github.com>
2020-11-09 19:19:03 -0600

    Update res_gcn.py

--- semisupervised_TU/pre-training/res_gcn.py
+++ semisupervised_TU/pre-training/res_gcn.py
@@ -221,12 +221,21 @@ class ResGCN(torch.nn.Module):
         x1_abs = x1.norm(dim=1)
         x2_abs = x2.norm(dim=1)
 
+        '''
         sim_matrix = torch.einsum('ik,jk->ij', x1, x2) / torch.einsum('i,j->ij', x1_abs, x2_abs)
         sim_matrix = torch.exp(sim_matrix / T)
         pos_sim = sim_matrix[range(batch_size), range(batch_size)]
         self_sim = sim_matrix[range(batch_size), list(range(int(batch_size/2), batch_size))+list(range(int(batch_size/2)))]
         loss = pos_sim / (sim_matrix.sum(dim=1) - pos_sim - self_sim)
         loss = - torch.log(loss).mean()
+        '''
+        
+        sim_matrix = torch.einsum('ik,jk->ij', x1, x2) / torch.einsum('i,j->ij', x1_abs, x2_abs)
+        sim_matrix = torch.exp(sim_matrix / T)
+        pos_sim = sim_matrix[range(batch_size), range(batch_size)]
+        loss = pos_sim / (sim_matrix.sum(dim=1) - pos_sim)
+        loss = - torch.log(loss).mean()
+        
         return loss
 
 

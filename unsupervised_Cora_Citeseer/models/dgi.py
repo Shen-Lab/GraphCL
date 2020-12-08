7f2e7f7a8ef967e9013101814a86682df46ecee3
Aventador <yongduosui@gmail.com>
2020-12-08 22:56:29 +0800

    Update dgi.py

--- unsupervised_Cora_Citeseer/models/dgi.py
+++ unsupervised_Cora_Citeseer/models/dgi.py
@@ -14,37 +14,30 @@ class DGI(nn.Module):
 
     def forward(self, seq1, seq2, seq3, seq4, adj, aug_adj1, aug_adj2, sparse, msk, samp_bias1, samp_bias2, aug_type):
         
-        
-        
+        h_0 = self.gcn(seq1, adj, sparse)
         if aug_type == 'edge':
 
             h_1 = self.gcn(seq1, aug_adj1, sparse)
             h_3 = self.gcn(seq1, aug_adj2, sparse)
-            c = self.read(h_3, msk)
-            c = self.sigm(c)
 
         elif aug_type == 'mask':
 
             h_1 = self.gcn(seq3, adj, sparse)
             h_3 = self.gcn(seq4, adj, sparse)
-            c = self.read(h_3, msk)
-            c = self.sigm(c)
 
         elif aug_type == 'node' or aug_type == 'subgraph':
 
-            h_0 = self.gcn(seq1, adj, sparse)
-
             h_1 = self.gcn(seq3, aug_adj1, sparse)
             h_3 = self.gcn(seq4, aug_adj2, sparse)
-
-            c_1 = self.read(h_1, msk)
-            c_1= self.sigm(c_1)
-
-            c_3 = self.read(h_3, msk)
-            c_3= self.sigm(c_3)
-
+            
         else:
             assert False
+            
+        c_1 = self.read(h_1, msk)
+        c_1= self.sigm(c_1)
+
+        c_3 = self.read(h_3, msk)
+        c_3= self.sigm(c_3)
 
         h_2 = self.gcn(seq2, adj, sparse)
 
@@ -52,7 +45,6 @@ class DGI(nn.Module):
         ret2 = self.disc(c_3, h_0, h_2, samp_bias1, samp_bias2)
 
         ret = ret1 + ret2
-        # ret = self.disc(c, h_1, h_2, samp_bias1, samp_bias2)
         return ret
 
     # Detach the return variables

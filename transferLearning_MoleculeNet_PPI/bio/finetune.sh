13dd3e3706ef3e02f5e212ded0f9723f1ff8d5e1
Yuning You <46954127+yyou1996@users.noreply.github.com>
2021-01-04 21:05:44 -0600

    Update finetune.sh

--- transferLearning_MoleculeNet_PPI/bio/finetune.sh
+++ transferLearning_MoleculeNet_PPI/bio/finetune.sh
@@ -4,5 +4,5 @@ split=species
 ### for GIN
 for runseed in 0 1 2 3 4 5 6 7 8 9
 do
-python finetune.py --model_file models_graphcl/graphcl_80.pth --split $split --epochs 50 --device 0 --runseed $runseed --gnn_type gin --lr 1e-4
+python finetune.py --model_file models_graphcl/graphcl_80.pth --split $split --epochs 50 --device 0 --runseed $runseed --gnn_type gin --lr 1e-3
 done
